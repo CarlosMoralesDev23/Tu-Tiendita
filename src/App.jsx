b9d1b8c338608f0bd08483a9cd19fbc3d70b6969
CarlosMoralesDev23 <carlosmoralesdev23@gmail.com>
2025-05-25 16:30:57 -0300

    Se agrega validación para decrement en caso de que quantity sea 1

--- src/App.jsx
+++ src/App.jsx
@@ -76,9 +76,10 @@ function App() {
     }
 
     const decrementQuantity = (product) => {
-        setCart(
+
+        product.quantity === 1 ? (removeItemFromCart(product)) : (setCart(
             cart.map((item)=> item.id === product.id ? {...item, quantity: item.quantity -1 }: item)
-        )
+        ))
     }
 
     const removeItemFromCart = (product) => {
