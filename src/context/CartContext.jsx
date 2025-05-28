a1f80031a0eccefe16a0f64bfe95542996deb404
CarlosMoralesDev23 <carlosmoralesdev23@gmail.com>
2025-05-27 21:54:58 -0300

    Mejorando codigo d elas funciones del CartContext

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,78 +1,99 @@
-import {createContext, useState, useEffect} from "react";
+import { createContext, useState, useEffect } from "react";
 
+export const CartContext = createContext();
 
-
-export const CartContext = createContext()
-
-export const CartProvider = ( { children } ) => {
-
-    const [cart, setCart] = useState([])
-    const [isCartOpen, setIsCartOpen] = useState(false)
-    const [products, setProducts] = useState([])
-    const [error, setError] = useState(false)
-    const [loader, setLoader] = useState(true)
-    const [isAuthenticated, setIsAuthenticated] = useState(false)
+export const CartProvider = ({ children }) => {
+    const [cart, setCart] = useState([]);
+    const [isCartOpen, setIsCartOpen] = useState(false);
+    const [products, setProducts] = useState([]);
+    const [error, setError] = useState(false);
+    const [loader, setLoader] = useState(true);
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
 
     useEffect(() => {
-    
-        fetch('/data/products.json')
-        .then((resp)=> resp.json())
-        .then((data)=> {
-            setTimeout(() => {
-                setProducts(data); setLoader(false)
-            }, 1250);
-        })
-        .catch((error)=> {
-            console.log("Error: ", error); setLoader(false); setError(true)
-        })
-    }, [])
+        fetch("/data/products.json")
+            .then((resp) => resp.json())
+            .then((data) => {
+                setTimeout(() => {
+                    setProducts(data);
+                    setLoader(false);
+                }, 1250);
+            })
+            .catch((error_) => {
+                console.log("Error: ", error_);
+                setLoader(false);
+                setError(true);
+            });
+    }, []);
+
+    //* FUNCIONES Carrito
+    const handleOpenCart = () => setIsCartOpen(true);
+    const handleCloseCart = () => setIsCartOpen(false);
+
+    const addToCart = (productToAdd) => {
+        const isProductInCart = cart.find(
+            (item) => item.id === productToAdd.id
+        );
+
+        if (isProductInCart) {
+            if (isProductInCart.quantity < productToAdd.stock) {
+                setCart(
+                    cart.map((item) =>
+                        item.id === productToAdd.id
+                            ? { ...item, quantity: item.quantity + 1 }
+                            : item
+                    )
+                );
+            } else {
+                console.log(`Ya no hay mas ${productToAdd.name} disponibles`);
+                alert(`Ya no hay mas ${productToAdd.name} disponibles`);
+            }
+        } else {
+            setCart([...cart, { ...productToAdd, quantity: 1 }]);
+        }
+    };
+
+    const emptyCart = () => {
+        setCart([]);
+    };
+
+    const incrementQuantity = (productToIncrement) => {
+        const productInCart = cart.find(
+            (item) => item.id === productToAdd.id
+        );
 
 
-    //* FUNCIONES
-
-    const handleOpenCart = () => setIsCartOpen(true)
-    const handleCloseCart = () => setIsCartOpen(false)
-
-    const addToCart = (product)=>{
-
-        const isProduct = cart.find((item)=> item.id === product.id)
-
-        if (isProduct) {
-
-            setCart( cart.map((item) => item.id === product.id ? {...item, quantity: item.quantity+1}:item ));
-
-        }else{
-            setCart([...cart, {...product, quantity:1}])
+        if (productInCart && productInCart.quantity < productToIncrement.quantity ) {
+            setCart(
+                cart.map((item) =>
+                    item.id === product.id
+                        ? { ...item, quantity: item.quantity + 1 }
+                        : item
+                )
+            )
+        } else if (productInCart) {
+            console.log(`Ya no hay mas ${productToAdd.name} disponibles`);
+            alert(`Ya no hay mas ${productToAdd.name} disponibles`);
+            
         }
 
-        console.log(cart)
-    }
-
-    const emptyCart = ()=>{
-        setCart([])
-    }
-
-    const incrementQuantity = (product)=>{
-
-        (product.quantity < product.stock ) ? setCart(cart.map((item) =>
-            item.id === product.id
-                ? { ...item, quantity: item.quantity + 1 }
-                : item
-        )): null;
-
-    }
+    };
 
     const decrementQuantity = (product) => {
-
-        product.quantity === 1 ? (removeItemFromCart(product)) : (setCart(
-            cart.map((item)=> item.id === product.id ? {...item, quantity: item.quantity -1 }: item)
-        ))
-    }
+        product.quantity === 1
+            ? removeItemFromCart(product)
+            : setCart(
+                  cart.map((item) =>
+                      item.id === product.id
+                          ? { ...item, quantity: item.quantity - 1 }
+                          : item
+                  )
+              );
+    };
 
     const removeItemFromCart = (product) => {
         setCart(cart.filter((item) => (item.id !== product.id ? item : null)));
     };
-
 
     return (
         <CartContext.Provider
@@ -85,17 +106,14 @@ export const CartProvider = ( { children } ) => {
                 removeItemFromCart,
                 handleOpenCart,
                 emptyCart,
-                incrementQuantity, decrementQuantity, isAuthenticated, isCartOpen, handleCloseCart
+                incrementQuantity,
+                decrementQuantity,
+                isAuthenticated,
+                isCartOpen,
+                handleCloseCart,
             }}
         >
             {children}
         </CartContext.Provider>
     );
-
-
-}
-
-
-
-
-
+};
