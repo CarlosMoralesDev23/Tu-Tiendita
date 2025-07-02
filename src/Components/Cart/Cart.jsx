612b9351d55b4287a5caf8378c443e3e6f5c5aef
CarlosMoralesDev23 <carlosmoralesdev23@gmail.com>
2025-07-01 21:49:09 -0300

    Se saca alert de confirmación y se implemento un sweet alert - Modal

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,16 +1,15 @@
 import React, { useState, useContext } from "react";
+import Swal from "sweetalert2"; // Importa SweetAlert2
 
-import "../Cart/Cart.css"; 
+import "../Cart/Cart.css";
 import CartItem from "./CartItem";
-import EmptyAndPay from "../Cart/EmptyAndPay"
-import ModalPay from "../Cart/ModalPay"
+import EmptyAndPay from "../Cart/EmptyAndPay";
+import ModalPay from "../Cart/ModalPay";
 import { CartContext } from "../../context/CartContext";
 
-const Cart = ( ) => {
-
-    const {cart, isCartOpen, handleCloseCart, emptyCart} = useContext(CartContext)
-
-
+const Cart = () => {
+    const { cart, isCartOpen, handleCloseCart, emptyCart } =
+        useContext(CartContext);
 
     const [isPaymentModalOpen, setIsPaymentModalOpen] = useState(false);
 
@@ -23,19 +22,28 @@ const Cart = ( ) => {
     };
 
     const handleConfirmPayment = () => {
+        console.log("Pago confirmado a través de SweetAlert2!");
 
-        console.log("Pago confirmado a través de MUI Modal!");
+        // 1. Cerrar el modal de pago inmediatamente
+        handleClosePaymentModal();
 
-        alert("¡Gracias por tu compra!"); 
-
-        handleClosePaymentModal(); 
-
-        emptyCart();
-
-        setTimeout(() => {
-            handleCloseCart()
-        }, 100);
-
+        // 2. Mostrar SweetAlert2
+        Swal.fire({
+            icon: "success",
+            title: "¡Pago exitoso!",
+            text: "Gracias por tu compra.",
+            timer: 2000, // Duración en milisegundos (2 segundos)
+            timerProgressBar: true,
+            showConfirmButton: false,
+            didOpen: (toast) => {
+                toast.addEventListener("mouseenter", Swal.stopTimer);
+                toast.addEventListener("mouseleave", Swal.resumeTimer);
+            },
+        }).then(() => {
+            // 3. Esta función se ejecuta después de que SweetAlert2 se cierra
+            emptyCart(); // Vaciar el carrito
+            handleCloseCart(); // Cerrar el carrito lateral
+        });
     };
 
     let cartClassName = "cartContainer";
@@ -48,7 +56,10 @@ const Cart = ( ) => {
             <div className="cartHeader">
                 <h2>Carrito de compras</h2>
                 <button className="closeButton" onClick={handleCloseCart}>
-                    <i className="fa-solid fa-right-from-bracket" style={{backgroundColor:"black"}}></i>
+                    <i
+                        className="fa-solid fa-right-from-bracket"
+                        style={{ backgroundColor: "black" }}
+                    ></i>
                 </button>
             </div>
 
